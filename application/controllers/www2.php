78189b405de0950ab503528e236e16b8cdc1a503
rick99856 <s13113241@stu.edu.tw>
2015-11-24 00:46:51 +0800

    2015/11/24 before message

--- application/controllers/www2.php
+++ application/controllers/www2.php
@@ -20,7 +20,7 @@ class www2 extends CI_Controller {
 					echo '<meta http-equiv="refresh" CONTENT="1; url=member">';
 				}
 				else{
-					echo "帳號密碼輸入錯誤或市長號並未開通";
+					echo "帳號密碼輸入錯誤或是帳號並未開通";
 					echo '<meta http-equiv="refresh" CONTENT="1; url=www">';
 				}
 			}
